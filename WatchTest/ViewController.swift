0536e5187dd94ad3f52bcc573ac2e800964a487f
Igor Tudoran <igortudoran@me.com>
2019-08-27 12:04:32 +0300

    Initial Commit

---
+++ WatchTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchTest
+//
+//  Created by Igor Tudoran on 8/27/19.
+//  Copyright © 2019 DIGIS IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
